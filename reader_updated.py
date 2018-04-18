d27b9ff7eb96c3b909c78478ab35404c25490cdd
Siyu Bian <siyub2@illinois.edu>
2018-04-17 21:18:47 -0500

    siyubian:finish naive version of search and ranking

--- reader_updated.py
+++ reader_updated.py
@@ -1,13 +1,10 @@
 import json
 
 f = open('reviews_Beauty_5.json', 'r')
-asin = open('asin.txt', 'w')
-reviewText = open('reviewText.txt', 'w')
-overall = open('overall.txt', 'w')
-result = open('result.txt', 'w')
+
+auxiliary = open('auxiliary.txt', 'w')
+
 for line in f:
     data = json.loads(line)
-    asin.write(str(data['asin']) + '\n')
-    reviewText.write(str(data['reviewText']) + '\n')
-    overall.write(str(data['overall']) + '\n')
-    result.write(str(data['reviewText']) + '\t' + str(data['asin']) + '\t' + str(data['overall']) + '\n')
+    auxiliary.write(data['asin'] +'\t' +str(data['overall']) + '\n')
+auxiliary.close()
